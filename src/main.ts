bf6ae8988af35befc5018eb84a5f9beaf55d48d8
InJaEE <in11202@naver.com>
2021-01-08 18:06:47 +0900

    set validation, swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['debug', 'log', 'error'],
+  });
+  const options = new DocumentBuilder()
+    .setTitle('Board API Docs')
+    .setDescription('The Board API description')
+    .setVersion('1.0')
+    .addTag('board')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/module/user/dto/index.ts
+++ src/module/user/dto/index.ts
@@ -1,6 +1,11 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNumber()
   readonly id: number;
+  @IsString()
   readonly name: string;
+  @IsString()
   readonly email: string;
 }
 
